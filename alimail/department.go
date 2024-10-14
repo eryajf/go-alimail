71fd433a83dc22f742f3c5cbf0c81359dfb69cb2
eryajf <eryajf@163.com>
2024-10-14 23:14:41 +0800

    基本完善基础交互能力

--- alimail/department.go
+++ alimail/department.go
@@ -162,3 +162,76 @@ func (d *DepartmentService) Delete(ctx context.Context, deptId string) error {
 
 	return parseAPIError(resp)
 }
+
+// 获取部门下用户列表的参数，三个字段，部门ID，页码，每页数量
+type ListDepartmentUsersReq struct {
+	ID     string `json:"id"`     // 部门ID
+	Offset int    `json:"offset"` // 分页偏移
+	Limit  int    `json:"limit"`  // 分页大小，最大100
+}
+
+// ListDepartmentUsersRsp 部门下用户列表的返回
+type ListDepartmentUsersRsp struct {
+	Users []User `json:"users"`
+	Total int    `json:"total"`
+}
+
+// GetDepartmentUsers 获取部门内的用户列表
+func (d *DepartmentService) GetDepartmentUsers(ctx context.Context, req ListDepartmentUsersReq) (rst ListDepartmentUsersRsp, err error) {
+	if req.Limit > 100 {
+		return rst, fmt.Errorf("limit can't be more than 100")
+	}
+	path := fmt.Sprintf("/v2/departments/%s/users?offset=%d&limit=%d", req.ID, req.Offset, req.Limit)
+
+	resp, err := d.doRequest(ctx, MethodGet, path, BaseHeader, nil)
+	if err != nil {
+		return rst, fmt.Errorf("request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		var dataObj ListDepartmentUsersRsp
+		if err := json.NewDecoder(resp.Body).Decode(&dataObj); err != nil {
+			return rst, fmt.Errorf("failed to decode response: %w", err)
+		}
+		return dataObj, nil
+	}
+	return rst, parseAPIError(resp)
+}
+
+// 获取部门下部门列表的参数，三个字段，部门ID，页码，每页数量
+type ListDepartmentDeptsReq struct {
+	ID      string `json:"id"`      // 部门ID
+	Offset  int    `json:"offset"`  // 分页偏移
+	Limit   int    `json:"limit"`   // 分页大小，最大100
+	Managed bool   `json:"managed"` // 是否只返回当前登录角色可管理的部门
+}
+
+// ListDepartmentDeptRsp 部门下用户列表的返回
+type ListDepartmentDeptsRsp struct {
+	Departments []Department `json:"departments"`
+	Total       int          `json:"total"`
+}
+
+// GetDepartmentDepts 获取部门内的子部门列表
+func (d *DepartmentService) GetDepartmentDepts(ctx context.Context, req ListDepartmentDeptsReq) (rst ListDepartmentDeptsRsp, err error) {
+	if req.Limit > 100 {
+		return rst, fmt.Errorf("limit can't be more than 100")
+	}
+	path := fmt.Sprintf("/v2/departments/%s/departments?offset=%d&limit=%d&managed=%t", req.ID, req.Offset, req.Limit, req.Managed)
+
+	resp, err := d.doRequest(ctx, MethodGet, path, BaseHeader, nil)
+	if err != nil {
+		return rst, fmt.Errorf("request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		var dataObj ListDepartmentDeptsRsp
+		if err := json.NewDecoder(resp.Body).Decode(&dataObj); err != nil {
+			return rst, fmt.Errorf("failed to decode response: %w", err)
+		}
+		return dataObj, nil
+	}
+	return rst, parseAPIError(resp)
+}
